c795198e45cf6a28d112a1c826761a79d67ded93
Idan Lev <idan.93lev@gmail.com>
2023-01-15 17:27:52 +0200

    test function in apple and orange code challenge file

--- apple_and_orange.js
+++ apple_and_orange.js
@@ -1,27 +1,22 @@
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
-  const applesLength = apples.length;
-  const orangesLength = oranges.length;
   let applesCounter = 0;
   let orangesCounter = 0;
 
-  const length = applesLength > orangesLength ? applesLength : orangesLength;
-
-  for (let i = 0; i < length; i++) {
-    if(applesLength !== i+2) {
-      const position = apples[i] + a;
-      if(position >= s && position <= t) {
-        applesCounter++;
-      }
-    }
-    if(orangesLength !== i+2) {
-      const position = oranges[i] + b;
-      if(position >= s && position <= t) {
-        orangesCounter++;
-      }
+  for (let i = 0; i < apples.length; i++) {
+    const position = apples[i] + a;
+    if (position >= s && position <= t) {
+      applesCounter++;
     }
   }
 
-  return (applesCounter, orangesCounter)
+  for (let i = 0; i < oranges.length; i++) {
+    const position = oranges[i] + b;
+    if (position >= s && position <= t) {
+      orangesCounter++;
+    }
+  }
+
+  return orangesCounter, applesCounter;
 }
 
 console.log(countApplesAndOranges(7, 11, 5, 15, [-2, 2, 1], [5, -6]));
