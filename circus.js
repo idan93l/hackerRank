90786cbb061f56b734a1bcff880981bf93df39f1
Idan Lev <idan.93lev@gmail.com>
2023-02-12 23:07:30 +0200

    fixed appearence

--- circus.js
+++ circus.js
@@ -1,17 +1,17 @@
-const circus = (x1,v1,x2,v2) => {
+const circus = (x1, v1, x2, v2) => {
   let d1 = x1 > x2 ? x1 : x2;
   let d2 = x1 < x2 ? x1 : x2;
   const dif = d1 - d2;
   const s1 = d1 === x1 ? v1 : v2;
   const s2 = d2 === x2 ? v2 : v1;
-  while(d1 > d2) {
-    d1+=s1;
-    d2+=s2;
-    if(d1 - d2 === dif || d1 - d2 > dif) {
+  while (d1 > d2) {
+    d1 += s1;
+    d2 += s2;
+    if (d1 - d2 === dif || d1 - d2 > dif) {
       break;
     }
   }
-  return d1 === d2 ? 'YES' : 'NO'
-}
+  return d1 === d2 ? "YES" : "NO";
+};
 
-console.log(circus(0,3,4,2));
+console.log(circus(0, 3, 4, 2));
--- diagonalDifference.js
+++ diagonalDifference.js
@@ -2,8 +2,8 @@ function diagonalDifference(arr) {
   let ld = 0;
   let rd = 0;
   for (let i = 0; i < arr.length; i++) {
-      ld += arr[i][i];
-      rd += arr[i][arr.length - 1 - i];
+    ld += arr[i][i];
+    rd += arr[i][arr.length - 1 - i];
   }
   return Math.abs(ld - rd);
 }
