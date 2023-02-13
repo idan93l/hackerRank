d611b4e2f3a0cc718930330da145afac74cafdd7
Idan Lev <idan.93lev@gmail.com>
2023-02-13 16:07:28 +0200

    birthday cake candles change appearance

--- birthday_cake_candles.js
+++ birthday_cake_candles.js
@@ -2,9 +2,9 @@ function birthdayCakeCandles(candles) {
   const max = Math.max(...candles);
   let sum = 0;
   for (let i = 0; i < candles.length; i++) {
-      if (candles[i] === max) {
-          sum++;
-      }
+    if (candles[i] === max) {
+      sum++;
+    }
   }
   return sum;
 }
